73cc8063be1e20f699fed81aae25ba794bd422f8
Ritesh <riteshbirajdarstudent@gmail.com>
2026-09-13 20:13:31 +0530

    task1 completed

---
+++ task1.py
@@ -0,0 +1,8 @@
+name = "Ritesh"
+age = 21
+percentage = 85.5
+is_student = True
+print(name, type(name))
+print(age, type(age))
+print(percentage, type(percentage))
+print(is_student, type(is_student))
