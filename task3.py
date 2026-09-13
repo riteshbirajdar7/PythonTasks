191ecf0ab2cb032abadd73081f347db57f403fbe
Ritesh <riteshbirajdarstudent@gmail.com>
2026-09-13 20:22:59 +0530

    task 3 is completed

---
+++ task3.py
@@ -0,0 +1,7 @@
+# boolean casting
+a=bool(0) # 0 is false
+b=bool(1) # 1 is true
+c=bool("")# empty string is false
+d=bool("hello")
+e=bool(" ")# space is also a string , hence true
+print(a, b, c, d,e)
